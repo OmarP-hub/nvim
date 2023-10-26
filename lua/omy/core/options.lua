6e3d4f60d238caf4d07507c33d72b381249a80ab
OmarP-hub <omy250@gmail.com>
2023-10-26 00:14:21 -0700

    packer config

---
+++ lua/omy/core/options.lua
@@ -0,0 +1,28 @@
+local opt = vim.opt -- for conciseness
+
+-- line numbers
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
+-- line wrapping
+opt.wrap = true
+
+-- search setting
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+
+-- clipboard
+opt.clipboard = "unnamedplus"
