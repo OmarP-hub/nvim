76b676048d40ab61ad5aec32b56d1f2765c77f8b
OmarP-hub <omy250@gmail.com>
2023-11-07 12:01:54 -0700

    agregar configuracion de TeX

--- lua/omy/ftplugins/tex.lua
+++ lua/omy/ftplugins/tex.lua
@@ -3,7 +3,24 @@ return{
   ft = "tex",
 
   config = function ()
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = "skim"
+    vim.cmd[[
+      let g:vimtex_compiler_latexmk_engines = {
+          \ '_'                : '-xelatex', 
+          \}
+    ]]
     vim.cmd[[ let maplocalleader = "," ]]
+    vim.cmd[[
+      function! s:TexFocusVim() abort
+        silent execute "!open -a iTerm2"
+        redraw!
+      endfunction
+    ]]
+    vim.cmd[[
+      augroup vimtex_event_focus
+        au!
+        au User VimtexEventViewReverse call s:TexFocusVim()
+      augroup END
+    ]]
   end
 }
